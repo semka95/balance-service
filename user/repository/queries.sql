d3b2121cf4ddca233098053074aa24cb5dfe403f
Semyon Zernov <sem.z@protonmail.com>
2023-01-14 15:22:49 +0300

    feat: add queries and generate sqlc files

---
+++ user/repository/queries.sql
@@ -0,0 +1,20 @@
+-- name: GetUser :one
+SELECT * FROM users
+WHERE id = $1 LIMIT 1;
+
+-- name: GetUserByEmail :one
+SELECT * FROM users
+WHERE email = $1 LIMIT 1;
+
+-- name: UpdateBalance :one
+UPDATE users SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: CreateUser :one
+INSERT INTO users(
+    name, email, balance
+) VALUES (
+    $1, $2, $3
+)
+RETURNING *;
