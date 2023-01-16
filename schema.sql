71432da0b538ccd23ca025ea584f1c509c8610ec
Semyon Zernov <sem.z@protonmail.com>
2023-01-16 23:21:15 +0300

    feat: make email of user unique

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@ CREATE TYPE valid_status AS ENUM ('new', 'accepted', 'rejected', 'error');
 CREATE TABLE users (
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR (30) NOT NULL,
-    email VARCHAR (20) NOT NULL,
+    email VARCHAR (20) UNIQUE NOT NULL,
     balance DECIMAL(10, 2) NOT NULL CHECK (balance >= 0),
     created_at TIMESTAMP NOT NULL DEFAULT NOW()
 );
